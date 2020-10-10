5ea7fed8c20de56aaa4b87aa11bfb5748a411b98
RitSar <55022926+RitSar@users.noreply.github.com>
2020-10-10 11:47:01 +0530

    using hashrouter

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import "./App.css";
 import Home from "./components/pages/Home";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Contact from "./components/pages/Contact";
 import About from "./components/pages/About";
 
@@ -12,9 +12,9 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path="/portfolio" exact component={Home} />
-          <Route path="/portfolio/about" component={About} />
-          <Route path="/portfolio/contact" component={Contact} />
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
         </Switch>
       </Router>
     </>
