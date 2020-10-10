f434afe555c7dd92d14386feb24cfb9f0f405fda
RitSar <55022926+RitSar@users.noreply.github.com>
2020-10-10 19:49:36 +0530

    Updates

--- precache-manifest.005dfd939863d28768c2fadba7ebbb23.js
+++ precache-manifest.005dfd939863d28768c2fadba7ebbb23.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3657ad192564d5406e1cb4d12db094f2",
+    "revision": "ee45f1314520a84a9c7973f479acabb1",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "af427f466409e489125b",
-    "url": "/portfolio/static/css/main.986f3284.chunk.css"
+    "revision": "33968a8cd142eafc140a",
+    "url": "/portfolio/static/css/main.ae5fe85a.chunk.css"
   },
   {
     "revision": "51981178d2bd1e9bceb6",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.85dafa2b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "af427f466409e489125b",
-    "url": "/portfolio/static/js/main.97fe0462.chunk.js"
+    "revision": "33968a8cd142eafc140a",
+    "url": "/portfolio/static/js/main.8c20674d.chunk.js"
   },
   {
     "revision": "309a9e55c61ba5bff665",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.75c04dd447836835f8c1839c37754102.js"
+  "/portfolio/precache-manifest.005dfd939863d28768c2fadba7ebbb23.js"
 );
 
 self.addEventListener('message', (event) => {
