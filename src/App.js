06401976db6749c85e6400daf5b79a80255f6e0f
RitSar <55022926+RitSar@users.noreply.github.com>
2020-10-10 11:38:40 +0530

    updated app routes

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
+          <Route path="/portfolio" exact component={Home} />
+          <Route path="/portfolio/about" component={About} />
+          <Route path="/portfolio/contact" component={Contact} />
         </Switch>
       </Router>
     </>
